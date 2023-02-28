75346da092d6076b40adaa617395d502bcf75c43
Anton Chaban <71810009+antonchaban@users.noreply.github.com>
2023-02-28 20:37:21 +0200

    added camera movement

--- Assets/Scripts/Player/SoldierEntity.cs
+++ Assets/Scripts/Player/SoldierEntity.cs
@@ -1,4 +1,6 @@
 using System;
+using Core.Enums;
+using Core.Tools;
 using UnityEngine;
 
 namespace Player
@@ -8,16 +10,18 @@ namespace Player
     {
         [Header("HorizontalMovement")] 
         [SerializeField] private float _horizontalSpeed;
-        [SerializeField] private bool _faceRight;
+        [SerializeField] private Direction _direction;
 
-        [Header("Jump")] 
+        [Header("Jump")]
         [SerializeField] private float _jumpForce;
         [SerializeField] private float _gravityScale;
-
-        private Rigidbody2D _soldierRigidbody2D;
-
         private bool _isJumping;
         private float _startJumpVerticalPosition;
+
+        [SerializeField] private DirectionalCameraPair _cameras;
+        
+        private Rigidbody2D _soldierRigidbody2D;
+
 
         // Start is called before the first frame update
         private void Start()
@@ -44,6 +48,32 @@ namespace Player
             _startJumpVerticalPosition = transform.position.y;
         }
 
+        public void MoveHorizontally(float direction)
+        {
+            SetDirection(direction);
+            Vector2 velocity = _soldierRigidbody2D.velocity;
+            velocity.x = direction * _horizontalSpeed;
+            _soldierRigidbody2D.velocity = velocity;
+        }
+
+        private void SetDirection(float direction)
+        {
+            if (_direction == Direction.Right  && direction < 0 || _direction == Direction.Left && direction > 0)
+            {
+                Flip();
+            }
+        }
+
+        private void Flip()
+        {
+            transform.Rotate(0f, 180f, 0f);
+            _direction = _direction == Direction.Right ? Direction.Left : Direction.Right;
+            foreach (var cameraPair in _cameras.DirectionalCameras)
+            {
+                cameraPair.Value.enabled = cameraPair.Key == _direction;
+            }
+        }
+
         private void UpdateJump()
         {
             if (_soldierRigidbody2D.velocity.y < 0 && _soldierRigidbody2D.position.y <= _startJumpVerticalPosition)
@@ -57,29 +87,6 @@ namespace Player
             _isJumping = false;
             _soldierRigidbody2D.position = new Vector2(_soldierRigidbody2D.position.x, _soldierRigidbody2D.position.y);
             _soldierRigidbody2D.gravityScale = 0;
-        }
-
-
-        public void MoveHorizontally(float direction)
-        {
-            SetDirection(direction);
-            Vector2 velocity = _soldierRigidbody2D.velocity;
-            velocity.x = direction * _horizontalSpeed;
-            _soldierRigidbody2D.velocity = velocity;
-        }
-
-        private void SetDirection(float direction)
-        {
-            if (_faceRight && direction < 0 || !_faceRight && direction > 0)
-            {
-                Flip();
-            }
-        }
-
-        private void Flip()
-        {
-            transform.Rotate(0f, 180f, 0f);
-            _faceRight = !_faceRight;
         }
     }
 }
